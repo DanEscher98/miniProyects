d0542cbc961b30b3ffa5c4f09ccc7533b0a8e723
DanEscher98 <danyiel5978@gmail.com>
2021-11-17 21:21:18 +0000

    new file:   bootrunner.sh 	new file:   src/vga_buffer.rs

---
+++ SystemsProg/first_ross/src/vga_buffer.rs
@@ -0,0 +1,145 @@
+use volatile::Volatile;
+use core::fmt;
+/* The VGA text buffer is a special memory area mapped to the VGA
+     * hardware that contains the contents displayed on screen. It
+     * normally consists of 25 lines that each contain 80 character 
+     * cells. The buffer is located at address 0xb8000. */
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+// This guarantess the correct field ordering like in a C struct
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]    
+// Ensure that it has the same emory layout as its single filed
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT]
+}
+
+pub struct Writer {
+    // Keeps track of the current position in the last row
+    column_position: usize,
+    color_code: ColorCode,
+    // Explicit lifetime, specifies that the reference is valid for
+    // the whole program run time (true for the VGA text buffer)
+    buffer: &'static mut Buffer 
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, string: &str) -> fmt::Result {
+        self.write_string(string);
+        Ok(())
+    }
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+                let color_code = self.color_code;
+
+                // Using the write method, instead of normal assignment,
+                // we guarantee that the compiler will never optimize
+                // away this write.
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, message: &str) {
+        for byte in message.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe) // a square character
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank)
+        }
+    }
+}
+
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) }
+        // 1st: cast the integer 0xb8000 as an mutable raw pointer
+        // 2nd: convert it to a mutable reference by dereferencing
+        //      and inmediately borrowing it again (&mut)
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    writer.write_string("World!");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+}
