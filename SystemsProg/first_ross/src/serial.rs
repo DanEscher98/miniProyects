4ab17cfef18405f98b35de06ff3d06e05bbd4954
DanEscher98 <danyiel5978@gmail.com>
2021-11-22 01:09:20 -0600

    modified:   src/lib.rs 	modified:   src/main.rs 	modified:   src/serial.rs 	modified:   src/vga_buffer.rs
     Untracked files:
    	../minigrep/

--- SystemsProg/first_ross/src/serial.rs
+++ SystemsProg/first_ross/src/serial.rs
@@ -16,7 +16,15 @@ lazy_static! {
 #[doc(hidden)]
 pub fn _print(args: ::core::fmt::Arguments) {
     use core::fmt::Write;
-    SERIAL1.lock().write_fmt(args).expect("Printing to serial failed");
+    use x86_64::instructions::interrupts;
+    
+    // This fix the Deadlock error when printing asynchronously
+    interrupts::without_interrupts(|| {
+        SERIAL1
+            .lock()
+            .write_fmt(args)
+            .expect("Printing to serial failed");
+    });
 }
 
 /// Prints to the host through the serial interface.
