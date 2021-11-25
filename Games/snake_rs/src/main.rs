95cff3b567155b41926b14443b3c34e3fe4052cb
Daniel Colin <danyiel5978@gmail.com>
2021-11-24 22:57:31 -0600

    Spawning food works

--- Games/snake_rs/src/main.rs
+++ Games/snake_rs/src/main.rs
@@ -1,10 +1,35 @@
 use bevy::prelude::*;
+use bevy::render::pass::ClearColor;
+use rand::prelude::random;
+use bevy::core::FixedTimestep;
+
+const ARENA_WIDTH: u32 = 15;
+const ARENA_HEIGHT: u32 = 15;
 
 fn main() {
     App::build()
+        .insert_resource(WindowDescriptor {
+            title: "Rusty Snake!".to_string(),
+            width: 500.0,
+            height: 500.0,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
         .add_system(snake_movement.system())
+        .add_system(snake_movement.system())
+        .add_system_set_to_stage(
+            CoreStage::PostUpdate,
+            SystemSet::new()
+                .with_system(position_translation.system())
+                .with_system(size_scaling.system())
+        )
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.0))
+                .with_system(food_spawner.system())
+        )
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -14,12 +39,14 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.insert_resource(Materials {
         head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+        food_material: materials.add(Color::rgb(1.0, 0.0, 1.0).into())
     });
 }
 
 struct SnakeHead;
 struct Materials {
     head_material: Handle<ColorMaterial>,
+    food_material: Handle<ColorMaterial>
 }
 
 fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
@@ -29,27 +56,98 @@ fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
             sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
         })
-        .insert(SnakeHead);
+        .insert(SnakeHead)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
 }
 
 fn snake_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    // he With type allows us to say “I want entities that have a snake head, but 
-    // I don’t care about the snake head component itself, just give me the transform”.
-    mut head_positions: Query<&mut Transform, With<SnakeHead>>
+    // The With type allows us to say “I want entities that have a
+    // snake head, but I don’t care about the snake head component
+    // itself, just give me the transform”.
+    mut head_positions: Query<&mut Position, With<SnakeHead>>,
 ) {
-    for mut transform in head_positions.iter_mut() {
+    for mut pos in head_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= 2.0;
+            pos.x -= 1;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += 2.0;
+            pos.x += 1;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= 2.0;
+            pos.y -= 1;
         }
         if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += 2.0;
+            pos.y += 1;
         }
+    }
+}
+
+struct Food;
+
+fn food_spawner(
+    mut commands: Commands,
+    materials: Res<Materials>
+) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.food_material.clone(),
+            ..Default::default()
+        })
+        .insert(Food)
+            .insert(Position {
+                x: (random::<f32>() * ARENA_WIDTH as f32) as i32,
+                y: (random::<f32>() * ARENA_HEIGHT as f32) as i32
+            })
+        .insert(Size::square(0.8));
+}
+
+#[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Sprite)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut sprite) in q.iter_mut() {
+        sprite.size = Vec2::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32
+        );
+    }
+}
+
+fn position_translation(
+    windows: Res<Windows>,
+    mut q: Query<(&Position, &mut Transform)>
+) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let title_size = bound_window / bound_game;
+        pos / bound_game * bound_window
+            - (bound_window / 2.0) + (title_size / 2.0)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0
+        );
     }
 }
