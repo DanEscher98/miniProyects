c6ea298782314d94dbec5cdbc8223eeacb50a03f
Daniel Colin <danyiel5978@gmail.com>
2021-11-28 03:21:38 -0600

    modified:   src/main.rs 	modified:   src/memory.rs

--- SystemsProg/first_ross/src/memory.rs
+++ SystemsProg/first_ross/src/memory.rs
@@ -4,8 +4,13 @@ use bootloader::bootinfo::{
 };
 use x86_64::{
     structures::paging::{
+        Page,
+        OffsetPageTable,
         PageTable,
-        OffsetPageTable
+        PhysFrame,
+        Mapper,
+        Size4KiB,
+        FrameAllocator
     },
     VirtAddr,
     PhysAddr
@@ -62,6 +67,29 @@ impl BootInfoFrameAllocator {
             memory_map,
             next: 0
         }
+    }
+    /// Returns an iterator over the usable frames specified in the memory map.
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        // get usable regions from memory map
+        let regions = self.memory_map.iter();
+        let usable_regions = regions
+            .filter(|r| r.region_type == MemoryRegionType::Usable);
+        // map each region to its address range
+        let addr_ranges = usable_regions
+            .map(|r| r.range.start_addr()..r.range.end_addr());
+        // transform to an iterator of rame start addresses
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        // create `PhysFrame` types from the start addresses
+        frame_addresses.map(
+           |addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
     }
 }
 
