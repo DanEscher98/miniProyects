1fb592b4a4a6be40604ccf117586c4982bf6ad6d
Daniel Colin <danyiel5978@gmail.com>
2021-12-07 18:14:36 -0600

    working lmc

--- SystemsProg/lmc/src/main.rs
+++ SystemsProg/lmc/src/main.rs
@@ -1,19 +1,23 @@
 extern crate lmc;
 
-use clap::{Arg, App};
-use std::io::{self, BufRead, BufReader};
-use std::fs::File;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("Little Man Computer")
         .version("1.0")
-        .arg(Arg::with_name("file")
-            .short("f")
-            .long("file")
-            .help("Indicates the file path")
-            .takes_value(true))
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .help("Indicates the file path")
+                .takes_value(true),
+        )
         .get_matches();
-    println!("Hello, world!");
 
     // If --file is present, read the values from a file
     let reader: Box<dyn BufRead> = match matches.value_of("file") {
@@ -21,14 +25,15 @@ fn main() {
             let path = std::path::PathBuf::from(file_path);
             let file = File::open(path).expect("File not found");
             Box::new(BufReader::new(file))
-        },
-        None => Box::new(BufReader::new(io::stdin()))
+        }
+        None => Box::new(BufReader::new(io::stdin())),
     };
 
     // Fill mailbox
     let mut mailbox: [i32; 100] = [0; 100];
     for (i, value) in reader.lines().enumerate() {
-        mailbox[i] = value.unwrap()
+        mailbox[i] = value
+            .unwrap()
             .trim()
             .parse()
             .expect(&format!("NaN on line {}", i + 1));
