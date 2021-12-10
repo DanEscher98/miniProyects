d7b50b19201d46117273472f6a4dbd9382ebb813
Daniel Colin <danyiel5978@gmail.com>
2021-12-10 01:29:58 -0600

    modified:   vm_lc3/main.c

--- SystemsProg/vm_lc3/main.c
+++ SystemsProg/vm_lc3/main.c
@@ -45,6 +45,30 @@ enum { FL_POS = 1 << 0, FL_ZERO = 1 << 1, FL_NEG = 1 << 2 };
 uint16_t memory[UINT16_MAX];
 uint16_t reg[R_COUNT];
 
+// It extends a number to 16 bits, filling in 0's for positive numbers and 1's
+// for negative numbers, so that original values are preserved. Two's Complement
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+	if ((x >> (bit_count - 1)) & 1) {
+		x |= (0xFFFF << bit_count);
+	}
+	return x;
+}
+
+// Any time a value is written to a register, we need to update the flags to
+// indicate its sign.
+void update_flags(uint16_t r)
+{
+	if (reg[r] == 0) {
+		reg[R_COND] = FL_ZERO;
+	} else if (reg[r] >> 15) {
+		// a 1 in the left-most bit indicates negative
+		reg[R_COND] = FL_NEG;
+	} else {
+		reg[R_COND] = FL_POS;
+	}
+}
+
 int main(int argc, const char** argv)
 {
 	// LOAD ARGUMENTS
@@ -108,8 +132,22 @@ int main(int argc, const char** argv)
 			break;
 		}
 		case OP_ADD: { /* add */
-			break;
-		}
+			// destination register (DR)
+			uint16_t r0 = (instr >> 9) & 0x7;
+			// first operand (SR1)
+			uint16_t r1 = (instr >> 6) & 0x7;
+			// whether we are in immediate mode
+			uint16_t imm_flag = (instr >> 5) & 0x1;
+
+			if (imm_flag) {
+				uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+				reg[r0] = reg[r1] + imm5;
+			} else {
+				uint16_t r2 = instr & 0x7;
+				reg[r0] = reg[r1] + reg[r2];
+			}
+			update_flags(r0);
+		} break;
 		case OP_JUMP: { /* jump */
 			break;
 		}
