60bcf813b079827a7cd58cedc0372120739a4897
Daniel Colin <danyiel5978@gmail.com>
2021-12-12 11:00:52 -0600

    modified:   vm_lc3/main.c

--- SystemsProg/vm_lc3/main.c
+++ SystemsProg/vm_lc3/main.c
@@ -20,30 +20,63 @@ enum {
 };
 
 // INSTRUCTION SET
-enum {
-	OP_BR = 0, // branch
-	OP_JPR,	   // jump register
-	OP_LOAD,   // load
-	OP_ST,	   // store
-	OP_LDR,	   // load register
-	OP_STR,	   // store register
-	OP_LDI,	   // load indirect
-	OP_STI,	   // store indirect
-	OP_FREE,   // unused
-	OP_NOT,	   // bitwise not
-	OP_AND,	   // bitwise and
-	OP_ADD,	   // add
-	OP_JUMP,   // jump
-	OP_RES,	   // reserved
-	OP_LEA,	   // load effective address
-	OP_TRAP	   // execute trap
-};
+typedef enum {
+	OP_BR = 0,	  // branch
+	OP_ADD = 1,	  // add
+	OP_LOAD = 2,  // load
+	OP_ST = 3,	  // store
+	OP_LDR = 6,	  // load register
+	OP_STR = 7,	  // store register
+	OP_LDI = 10,  // load indirect
+	OP_STI = 11,  // store indirect
+	OP_FREE = 8,  // unused
+	OP_NOT = 9,	  // bitwise not
+	OP_AND = 5,	  // bitwise and
+	OP_JPR = 4,	  // jump register
+	OP_JUMP = 12, // jump
+	OP_RES = 13,  // reserved (illegal)
+	OP_LEA = 14,  // load effective address
+	OP_TRAP = 15  // execute trap
+} opcode;
 
 // CONDITION FLAGS
 enum { FL_POS = 1 << 0, FL_ZERO = 1 << 1, FL_NEG = 1 << 2 };
 
-uint16_t memory[UINT16_MAX];
-uint16_t reg[R_COUNT];
+uint16_t memory[UINT16_MAX]; // 65'535 == 0xFFFF
+uint16_t reg[R_COUNT];		 // 10
+uint16_t sign_extend(uint16_t, int);
+// swap
+void update_flags(uint16_t);
+// read image file
+// read image
+// check key
+// memory access
+// input buffering
+// handle interrupt
+void compute(void);
+
+int main(int argc, const char** argv)
+{
+	// LOAD ARGUMENTS
+	if (argc < 2) {
+		/* show usage string */
+		printf("lc3 [image-file] ...\n");
+		exit(2);
+	}
+	for (int i = 1; i < argc; i++) {
+		if (!read_image(argv[i])) {
+			printf("failed to load image: %s\n", argv[i]);
+			exit(1);
+		}
+	}
+
+	// SETUP
+
+	compute();
+
+	// SHUTDOWN
+	return EXIT_SUCCESS;
+}
 
 // It extends a number to 16 bits, filling in 0's for positive numbers and 1's
 // for negative numbers, so that original values are preserved. Two's Complement
@@ -69,23 +102,8 @@ void update_flags(uint16_t r)
 	}
 }
 
-int main(int argc, const char** argv)
+void compute()
 {
-	// LOAD ARGUMENTS
-	if (argc < 2) {
-		/* show usage string */
-		printf("lc3 [image-file] ...\n");
-		exit(2);
-	}
-	for (int i = 1; i < argc; i++) {
-		if (!read_image(argv[i])) {
-			printf("failed to load image: %s\n", argv[i]);
-			exit(1);
-		}
-	}
-
-	// SETUP
-
 	/* set the PC to starting position */
 	/* 0x3000 is the default */
 	enum { PC_START = 0x3000 };
@@ -94,20 +112,40 @@ int main(int argc, const char** argv)
 	bool running = true;
 	while (running) {
 		/* FETCH */
-		uint16_t instr = mem_read(reg[R_PC]++);
-		uint16_t op = instr >> 12;
+		uint16_t instruction = mem_read(reg[R_PC]++);
+		uint16_t op = instruction >> 12;
 
 		switch (op) {
 		case OP_BR: { /* branch */
 			break;
 		}
-		case OP_JPR: { /* jump register */
-			break;
-		}
+		case OP_ADD: { /* add */
+			// destination register (DR)
+			uint16_t r0 = (instruction >> 9) & 0x7;
+			// first operand (SR1)
+			uint16_t r1 = (instruction >> 6) & 0x7;
+			// whether we are in immediate mode
+			uint16_t imm_flag = (instruction >> 5) & 0x1;
+
+			if (imm_flag) {
+				uint16_t imm5 = sign_extend(instruction & 0x1F, 5);
+				reg[r0] = reg[r1] + imm5;
+			} else {
+				uint16_t r2 = instruction & 0x7;
+				reg[r0] = reg[r1] + reg[r2];
+			}
+			update_flags(r0);
+		} break;
 		case OP_LOAD: { /* load */
 			break;
 		}
 		case OP_ST: { /* store */
+			break;
+		}
+		case OP_JPR: { /* jump register */
+			break;
+		}
+		case OP_AND: { /* bitwise and */
 			break;
 		}
 		case OP_LDR: { /* load register */
@@ -116,38 +154,18 @@ int main(int argc, const char** argv)
 		case OP_STR: { /* store register */
 			break;
 		}
-		case OP_LDI: { /* load indirect */
-			break;
-		}
-		case OP_STI: { /* store indirect */
-			break;
-		}
 		case OP_FREE: { /* unused */
 			break;
 		}
 		case OP_NOT: { /* bitwise not */
 			break;
 		}
-		case OP_AND: { /* bitwise and */
+		case OP_LDI: { /* load indirect */
 			break;
 		}
-		case OP_ADD: { /* add */
-			// destination register (DR)
-			uint16_t r0 = (instr >> 9) & 0x7;
-			// first operand (SR1)
-			uint16_t r1 = (instr >> 6) & 0x7;
-			// whether we are in immediate mode
-			uint16_t imm_flag = (instr >> 5) & 0x1;
-
-			if (imm_flag) {
-				uint16_t imm5 = sign_extend(instr & 0x1F, 5);
-				reg[r0] = reg[r1] + imm5;
-			} else {
-				uint16_t r2 = instr & 0x7;
-				reg[r0] = reg[r1] + reg[r2];
-			}
-			update_flags(r0);
-		} break;
+		case OP_STI: { /* store indirect */
+			break;
+		}
 		case OP_JUMP: { /* jump */
 			break;
 		}
@@ -166,7 +184,4 @@ int main(int argc, const char** argv)
 		}
 		}
 	}
-
-	// SHUTDOWN
-	return EXIT_SUCCESS;
 }
