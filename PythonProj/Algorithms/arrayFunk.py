8b86c8ff6313e7ed4a4b933e1f6b2f93c43c352c
Danyiel Colin <danyiel5978@gmail.com>
2022-05-08 20:17:29 -0500

    modified:   PythonProj/Algorithms/EuclidAlg.py new file:   PythonProj/Algorithms/Output/z:(10-5j) n:4.png new file:   PythonProj/Algorithms/Output/z:(16+0j) n:4.png new file:   PythonProj/Algorithms/Output/z:(16-20j) n:5.png new file:   PythonProj/Algorithms/Output/z:(20+25j) n:8.png new file:   PythonProj/Algorithms/Output/z:(3+4j) n:5.png modified:   PythonProj/Algorithms/TeoProd.py modified:   PythonProj/Algorithms/arrayFunk.py modified:   PythonProj/Algorithms/contFractions.py modified:   PythonProj/Algorithms/dampedW.py modified:   PythonProj/Algorithms/deMoivre.py modified:   PythonProj/Algorithms/numerical_integration.py modified:   PythonProj/Algorithms/pascal.py modified:   PythonProj/Algorithms/quadraticF.py new file:   PythonProj/Algorithms/requirements.txt new file:   SystemsProg/extras/Makefile new file:   SystemsProg/extras/bidmaster.c new file:   SystemsProg/extras/bidslave.c new file:   SystemsProg/extras/sendtermios.py new file:   SystemsProg/extras/ttyecho.c new file:   SystemsProg/rs_simpleshell/Cargo.lock new file:   SystemsProg/rs_simpleshell/Cargo.toml new file:   SystemsProg/rs_simpleshell/src/main.rs modified:   WebPage/BasicWeb-I/home.html

--- PythonProj/Algorithms/arrayFunk.py
+++ PythonProj/Algorithms/arrayFunk.py
@@ -1,29 +1,33 @@
-import numpy as np
 import decimal
 from math import trunc
 
+import numpy as np
+
+
 def select(list, a, b):
     result = []
-    for i in range(a, b+1):
+    for i in range(a, b + 1):
         j = i
-        while(j>len(list)-1):
+        while j > len(list) - 1:
             j = j - len(list)
         result.append(list[j])
     return result
+
 
 def frange(start, final, interval):
     d = decimal.Decimal(str(interval))
     r = abs(d.as_tuple().exponent)
     num = []
-    while start<final:
+    while start < final:
         num.append(start)
         start += interval
-    #num.append(final)
+    # num.append(final)
     num = np.array([round(x, r) for x in num])
     for i in range(0, len(num)):
-        if(num[i]==trunc(num[i])):
-    		num[i]=int(num[i])
+        if num[i] == trunc(num[i]):
+            num[i] = int(num[i])
     return num
+
 
 def fact(n):
     ans = 1
